f14e6639b834e3fcc718e39f513d43383484852e
JP Canindo <jp36tin@gmail.com>
2023-01-11 21:54:07 +0800

    update main & post routes

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,8 @@ router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
+// router.post("/createPost", upload.array("photos", 5), postsController.createPost);
+
 router.put("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePost/:id", postsController.deletePost);
